201587ae1ded6553265e4cc1a3c39df3e8512c31
Kirill Purgenka <famiconbit@yahoo.com>
2019-05-18 18:09:32 +0700

    mock service

--- server.js
+++ server.js
@@ -2,25 +2,27 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const getNumber = () => {
-  return 'TS' + parseInt(Math.random() * 1000) + 'T154';
-};
 
-/*
+const username = 'onpp_user';
+const host = '95.179.241.170';
+const password = '123123';
 
-await client.connect()
-
-const res = await client.query('SELECT $1::text as message', ['Hello world!'])
-console.log(res.rows[0].message) // Hello world!
-await client.end();*/
-
-
+const { Client } = require('pg');
+const conStringPri = `postgres://${username}:${password}@${host}/onpp`;
+const client = new Client({connectionString: conStringPri});
+client.connect();
+const getNumber = () => ('TS' + parseInt(Math.random() * 1000) + 'T154');
 app.get('/in', (req, res) => {
-  let number = getNumber();
-  let result = {
-      number: number
-  };
-  res.send(JSON.stringify(result));
+    let output = { number:  getNumber() };
+    const text = 'INSERT INTO onpp.user(number) VALUES($1) RETURNING *';
+    const values = [output.number];
+    client.query(text, values, (err, result) => {
+        if (err) {
+            res.send(JSON.stringify(err.stack));
+        } else {
+            res.send(JSON.stringify(output));
+        }
+    });
 });
 
 app.get('/out', (req, res) => {
